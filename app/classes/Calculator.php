bb4e4de860c1d987530007fea61fd80038e2667f
rakibqww <rakib456@gmail.com>
2022-02-03 15:16:20 +0600

    Class Update 12

--- app/classes/Calculator.php
+++ app/classes/Calculator.php
@@ -1,24 +1,77 @@
 <?php
 namespace App\classes;
 use App\classes\User;
-class Calculator extends User{
-
-    public $firstNumber = 10;
-    protected $secondNumber = 20;
-    private $operator = 30;
+class Calculator extends User{ //Child extends Parent=> Inheritance
+//=>encapsulation
+    public $firstNumber ;
+    protected $secondNumber ;
+    private $operator ;
     public $result;
     public $user;
 
+    public function __construct($data)    {
+      //  echo is_array($data) ? 'Data is Array' : 'Data is single Variable';
+        // '?' => if , ':' else => Ternary operation
+
+//        $this->firstNumber = 10;
+//        $this->secondNumber = 20;
+//        $this->operator = '+';
+
+        $this->firstNumber = $data['first_number'];
+        $this->secondNumber = $data['second_number'];
+        $this->operator = $data['operator'];
+
+    }
+
+
     public function index(){
-       $this->user = new User();
-      echo $this->user->city;
+//       $this->user = new User();
+//      echo $this->user->country; // protected Property
+      //echo $this->profile(); Private Property
+
+//    public function index(){   encapsulation
+//        $this->user = new User();
+//        $this->user->
+//     echo  $this->add();
+//     echo  $this->sub();
+//     echo  $this->multiplication();
+//     echo  $this->division();
+
+        switch ($this->operator) {
+            case '+':
+                $this->result = $this->add();
+                //echo/return $this->add();
+                break;
+            case '-' :
+                $this->result = $this-> sub();
+                break;
+            case '*' :
+                $this->result = $this-> multiplication();
+                break;
+            case '/' :
+                $this->result = $this-> division();
+                break;
+            case '%' :
+                $this->result = $this-> remainder();
+                break;
+        }
+       return $this->result;
     }
 
-    protected function one(){
-        echo 'Hello World One';
-    } 
-
-    private function two(){
-        echo 'Hello World Two';
+    protected function add(){
+      return  $this->firstNumber + $this->secondNumber;
     }
+    protected function sub(){
+        return $this->firstNumber - $this->secondNumber;
+    }
+    protected function multiplication(){
+        return $this->firstNumber * $this->secondNumber;
+    }
+    protected function division(){
+        return $this->firstNumber / $this->secondNumber;
+    }
+    protected function remainder(){
+        return $this->firstNumber % $this->secondNumber;
+    }
+
 }
---
+++ index_Pre.php
@@ -0,0 +1,12 @@
+<?php
+
+////
+//require_once 'vendor/autoload.php';
+//use App\classes\Calculator;
+////
+//$calculator = new Calculator('Rakib Hasan');
+//////$calculator->index();  Methord call
+//////echo $calculator->firstNumber; property call
+////
+////
+//$calculator->index();
